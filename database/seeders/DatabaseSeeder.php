158a8a67774e8022296d1d8dc8110baf84466270
bianca-bansos <bianca-bansos@ssh1.paris1.alwaysdata.com>
2025-11-20 01:08:58 +0100

    Save local changes to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+// Tambahkan ini untuk men-seed semua tabel lainnya
+        $this->call([
+            CreateWargaSeeder::class,
+            CreateFirstUser::class,
+            CreateProgramSeeder::class,
+            CreatePendaftarSeeder::class, // relasi proyek → lokasi
         ]);
     }
 }
